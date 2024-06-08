16f5bd5abc91e77fbf6b819bb594bb3d1650bd6f
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-06-08 11:35:24 +0600

    arguments

--- JavaScript/p23.js
+++ JavaScript/p23.js
@@ -1,1 +1,9 @@
-console.log("Hello linux");
+// console.log("Hello linux");
+
+function add(num1, num2){
+    console.log(num1, num2);
+    console.log(arguments);
+    console.log("the 5th value is: ", arguments[4]);
+}
+
+add(12, 34, 45, 67, 89);
