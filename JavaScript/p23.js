390eca7766b5472bec50ce500711d86e521d31f1
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-06-08 11:46:53 +0600

    for of loop

--- JavaScript/p23.js
+++ JavaScript/p23.js
@@ -1,9 +1,15 @@
 // console.log("Hello linux");
 
-function add(num1, num2){
-    console.log(num1, num2);
-    console.log(arguments);
-    console.log("the 5th value is: ", arguments[4]);
-}
+// function add(num1, num2){
+//     console.log(num1, num2);
+//     console.log(arguments);
+//     console.log("the 5th value is: ", arguments[4]);
+// }
 
-add(12, 34, 45, 67, 89);
+// add(12, 34, 45, 67, 89);
+
+let abcArray = [2, 3, 4, 5, 6, 7, 8, 9];
+
+for(let element of abcArray){
+    console.log(element);
+}
