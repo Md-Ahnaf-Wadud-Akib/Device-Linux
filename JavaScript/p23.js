f07c2d3901d64faf2a63961eeb8e1f6918124076
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-06-08 12:01:09 +0600

    for of object

--- JavaScript/p23.js
+++ JavaScript/p23.js
@@ -8,8 +8,24 @@
 
 // add(12, 34, 45, 67, 89);
 
-let abcArray = [2, 3, 4, 5, 6, 7, 8, 9];
+// let abcArray = [2, 3, 4, 5, 6, 7, 8, 9];
 
-for(let element of abcArray){
-    console.log(element);
+// for(let element of abcArray){
+//     console.log(element);
+// }
+
+function productElements(products){
+    for(let product of products){
+        console.log(product);
+    }
 }
+
+let input =[
+    {name: "Samsung", camera: 50, price: 12000, ram: 8},
+    {name: "Walton", camera: 13, price: 7000, ram: 2},
+    {name: "Oppo", camera: 36, price: 92000, ram: 4},
+    {name: "Nokia", camera: 25, price: 22000, ram: 6},
+    {name: "iphone", camera: 13, price: 2000, ram: 9}
+];
+
+productElements(input);
