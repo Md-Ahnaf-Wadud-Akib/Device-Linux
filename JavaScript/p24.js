431a41a32d9d7618c5ebbe3eb2cc35c16f4b80b4
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-06-11 01:07:35 +0600

    DOM d2

--- JavaScript/p24.js
+++ JavaScript/p24.js
@@ -1,2 +1,19 @@
 // console.log("hello p24.js")
 
+// let paragraph = document.getElementsByTagName("p");
+// console.log(paragraph);
+
+const student = {
+    name: "Nikko",
+    id: 23,
+    friends: function (marks){
+        if(studentMarks > 60){
+            console.log("let's be friends");
+        }
+        else{
+            console.log("not keeping bad company");
+        }
+    },
+
+    address: "Rajshahi"
+}
