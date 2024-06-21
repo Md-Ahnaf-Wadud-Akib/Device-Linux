7a16fc094de50f45f5fd84c462428f649b1e7416
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-06-21 21:42:09 +0600

    DOM

--- JavaScript/p24.js
+++ JavaScript/p24.js
@@ -3,3 +3,14 @@
 // let paragraph = document.getElementsByTagName("p");
 // console.log(paragraph);
 
+// let liElements = document.getElementsByTagName("li");
+// for(li of liElements){
+//     console.log(li);
+// }
+
+// let pineapple = document.getElementById("change-text");
+// console.log(pineapple.innerText);
+
+let textInner = document.querySelector(".section-header p");
+console.log(textInner.style.color = "red");
+
