6340b999b9fb724220c9461352f201b2ba2a9680
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-06-21 20:36:30 +0600

    Eid break over

--- JavaScript/p24.js
+++ JavaScript/p24.js
@@ -3,17 +3,3 @@
 // let paragraph = document.getElementsByTagName("p");
 // console.log(paragraph);
 
-const student = {
-    name: "Nikko",
-    id: 23,
-    friends: function (marks){
-        if(studentMarks > 60){
-            console.log("let's be friends");
-        }
-        else{
-            console.log("not keeping bad company");
-        }
-    },
-
-    address: "Rajshahi"
-}
