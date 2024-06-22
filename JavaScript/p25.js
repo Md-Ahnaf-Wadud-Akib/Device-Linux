84e296fae2f6878a1b04950f02452517ac2cd9b8
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-06-22 23:51:02 +0600

    darkMode whiteMode

---
+++ JavaScript/p25.js
@@ -0,0 +1,9 @@
+function darkMode(){
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+}
+
+function whiteMode(){
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+}
