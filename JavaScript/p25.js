1f861295dd19c7c815a9cead20d56765ee7bdbca
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-06-23 03:44:32 +0600

    inputBox

--- JavaScript/p25.js
+++ JavaScript/p25.js
@@ -26,6 +26,33 @@ function orangeColor(){
 }
 orangeMode.addEventListener("click", orangeColor);
 
+function changeText(){
+    let thankYou = document.getElementById("thank-you");
+    thankYou.innerText = "Thank you for clicking";
+}
+
+let clickMe = document.getElementById("click-me");
+clickMe.addEventListener("click", changeText);
+
+// Div1
+
+let divStyle = document.getElementById("div1");
+divStyle.style.padding = "30px 0px";
+
+
+let textBox = document.getElementById("text-box");
+
+
+function text(){
+    inputText.innerText = textBox.value;    
+}
+
+let submitText = document.getElementById("submit");
+
+let inputText = document.getElementById("input-text");
+
+submitText.addEventListener("click", text);
+
 
 
 
