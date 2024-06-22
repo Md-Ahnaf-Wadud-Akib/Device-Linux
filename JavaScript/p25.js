b661dbf4a86de3c2acf95423c22de3e38911c34d
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-06-23 02:52:51 +0600

    addEventListener

--- JavaScript/p25.js
+++ JavaScript/p25.js
@@ -7,3 +7,26 @@ function whiteMode(){
     document.body.style.backgroundColor = "white";
     document.body.style.color = "black";
 }
+
+let purple = document.getElementById("purpleMode");
+function purpleBg(){
+    document.body.style.backgroundColor = "purple";
+    document.body.style.color = "black";
+}
+purple.onclick = purpleBg;
+
+let pinkMode = document.getElementById("pink-mode");
+pinkMode.addEventListener("click", function(){
+    document.body.style.backgroundColor = "pink";
+})
+
+let orangeMode = document.getElementById("orange-mode");
+function orangeColor(){
+    document.body.style.backgroundColor = "orange";
+}
+orangeMode.addEventListener("click", orangeColor);
+
+
+
+
+
