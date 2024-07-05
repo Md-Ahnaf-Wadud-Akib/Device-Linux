b41ae0f2dd05d085b5f28c51c2f1bb01e68680f1
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-07-06 01:26:39 +0600

    logIn page added

---
+++ JavaScript/simpleBankingSystem.js
@@ -0,0 +1,14 @@
+let submit = document.getElementById("submit-button");
+submit.addEventListener("click", function () {
+  let mailBox = document.getElementById("mail-box");
+  let mail = mailBox.value;
+
+  let passwordBox = document.getElementById("password-box");
+  let password = passwordBox.value;
+
+  if (mail === "protiva@gmail.com" && password === "12345") {
+    window.location.href = "welcomeToBank.html";
+  } else {
+    alert("Wrong user name or password");
+  }
+});
