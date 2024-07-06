8788a18b6a51fc1e0faf04f17cd808b2b0df78b6
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-07-06 23:16:48 +0600

    withdraw added

--- JavaScript/bank.js
+++ JavaScript/bank.js
@@ -1,2 +1,13 @@
-let deposit = document.getElementById("out-box");
-console.log(deposit);
+let withdrawButton = document.getElementById("withdraw-button");
+withdrawButton.addEventListener("click", function () {
+  let withdrawValue = document.getElementById("withdraw-value");
+  let showWithdrawValue = document.getElementById("show-withdraw-value");
+  showWithdrawValue.innerText = withdrawValue.value;
+
+  let totalAmount = document.getElementById("total-amount");
+  if (withdrawValue.value < totalAmount.innerText) {
+    totalAmount.innerText = totalAmount.innerText - withdrawValue.value;
+  } else {
+    alert("Not enough money");
+  }
+});
