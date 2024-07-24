32a94d0d243fff84227d99d3ebe78b1dd6c03a79
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-07-24 20:28:11 +0600

    practiceProblems

---
+++ JavaScript/practiceProblems.js
@@ -0,0 +1,21 @@
+// let input =100;
+// for(let i = 0; i <= input; i++){
+    
+//     if(i % 3 === 0 && i % 5 === 0){
+//         console.log("FizzBuzz");
+//     }
+//     else if(i % 3 === 0){
+//         console.log("Fizz");
+//     }
+//     else if(i % 5 === 0){
+//         console.log("Buzz")
+//     }
+//     else{
+//         console.log(i);
+//     }
+// }
+
+let names = "Oreo";
+for(i=names.length-1; i >= 0; i--){
+    console.log(names[i]);
+}
