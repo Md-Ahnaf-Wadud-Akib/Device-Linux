a954c71edef5db83e6b1b0affd6e622a1efef8f7
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-08-06 22:30:37 +0600

    bug found

--- practice project/JS/login.js
+++ practice project/JS/login.js
@@ -1,15 +1,15 @@
 let darkMode = document.getElementById("dark-mode");
-darkMode.addEventListener("click", function dark(){
-    let body = document.body;
-    body.style.backgroundColor = "black";
-    darkMode.style.backgroundColor = "white";
-    darkMode.style.color = "black";
-    darkMode.innerText = "White Mode";
+darkMode.addEventListener("click", function() {
+  let body = document.body;
+  body.style.backgroundColor = "black";
+  darkMode.style.backgroundColor = "white";
+  darkMode.style.color = "black";
+  darkMode.innerText = "White Mode";
 
-    darkMode.addEventListener("click", function white(){
-        body.style.backgroundColor = "white";
-        darkMode.style.backgroundColor = "black";
-        darkMode.style.color = "white";
-        darkMode.innerText = "Dark Mode";
-    })
-})
+  darkMode.addEventListener("click", function() {
+    body.style.backgroundColor = "white";
+    darkMode.style.backgroundColor = "black";
+    darkMode.style.color = "white";
+    darkMode.innerText = "Dark Mode";
+  });
+});
---
+++ practice project/JS/utility.js
@@ -0,0 +1,15 @@
+// function dark() {
+//   let body = document.body;
+//   body.style.backgroundColor = "black";
+//   darkMode.style.backgroundColor = "white";
+//   darkMode.style.color = "black";
+//   darkMode.innerText = "White Mode";
+// }
+
+// function white() {
+//   let body = document.body;
+//   body.style.backgroundColor = "white";
+//   darkMode.style.backgroundColor = "black";
+//   darkMode.style.color = "white";
+//   darkMode.innerText = "Dark Mode";
+// }
