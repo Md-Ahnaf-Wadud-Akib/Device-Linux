18279552a5afffc68ae45a047f0535189bf05624
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-10-25 16:44:57 +0600

    upgrading

--- upgrade/javaScript/isNaN.js
+++ upgrade/javaScript/isNaN.js
@@ -1,23 +1,31 @@
-// // console.log("Hello World");
+// // // console.log("Hello World");
 
-// let number = "Protiva";
+// // let number = "Protiva";
 
-// console.log(isNaN(number));
+// // console.log(isNaN(number));
 
-function sum(num1, num2){
-    let result = num1 + num2;
-    // console.log(num1, num2, result);
-    return result;
+// function sum(num1, num2){
+//     let result = num1 + num2;
+//     // console.log(num1, num2, result);
+//     return result;
+// }
+
+// const input1 = 2
+// const input2 = 3;
+
+// let output = sum(input1, input2);
+// console.log(`The sum of ${input1} and ${input2} is ${output}`);
+
+// let something = `Hello World,
+// Welcome to the repository
+// Thank You For Visiting`;
+
+// console.log(something);
+
+function multiply(num1, num2) {
+  return num1 * num2;
 }
-
-const input1 = 2
-const input2 = 3;
-
-let output = sum(input1, input2);
-console.log(`The sum of ${input1} and ${input2} is ${output}`);
-
-let something = `Hello World,
-Welcome to the repository
-Thank You For Visiting`;
-
-console.log(something);
+let input1 = 2;
+input2 = 3;
+const result = multiply(2, 3);
+console.log(`The multiplication of ${input1} and ${input2} is: ${result}`);
