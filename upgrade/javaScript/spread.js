5a0b01c02c6ac4d271bbeb105e670a616849387d
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-10-25 18:25:36 +0600

    spread

--- upgrade/javaScript/spread.js
+++ upgrade/javaScript/spread.js
@@ -10,9 +10,16 @@
 //   return maxElement;
 // }
 
-const abcArray = [23, 97, 56, 74, 568, 78, 54, 37, 26];
-const maxValue = Math.max(...abcArray);
-console.log(maxValue);
+// const abcArray = [23, 97, 56, 74, 568, 78, 54, 37, 26];
+// const maxValue = Math.max(...abcArray);
+// console.log(maxValue);
 // const result = maxValue(abcArray);
 // console.log(`The max value of ${abcArray} is: ${result}`);
 
+
+const ahnaf = ["mango", "potato", "spice", "Singara"];
+const akib = ["chicken", "rice"];
+
+// Combine both arrays using the spread operator
+const ahnafAkib = [...ahnaf, ...akib];
+console.log(ahnafAkib);
