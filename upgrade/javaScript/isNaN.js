a6a1a3d5656a31622b1bcf492af85a811eb4e591
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-10-25 16:18:44 +0600

    undefined

--- upgrade/javaScript/isNaN.js
+++ upgrade/javaScript/isNaN.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+// // console.log("Hello World");
+
+// let number = "Protiva";
+
+// console.log(isNaN(number));
+
+function sum(num1, num2){
+    let result = num1 + num2;
+    console.log(num1, num2, result);
+    return result;
+}
+
+const input1 = 2
+// const input2 = 3;
+
+sum(input1, input2);
