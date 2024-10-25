604a148274f90afed14d353372c00af48ca28a9c
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-10-25 16:35:31 +0600

    upgrading

--- upgrade/javaScript/isNaN.js
+++ upgrade/javaScript/isNaN.js
@@ -6,11 +6,18 @@
 
 function sum(num1, num2){
     let result = num1 + num2;
-    console.log(num1, num2, result);
+    // console.log(num1, num2, result);
     return result;
 }
 
 const input1 = 2
-// const input2 = 3;
+const input2 = 3;
 
-sum(input1, input2);
+let output = sum(input1, input2);
+console.log(`The sum of ${input1} and ${input2} is ${output}`);
+
+let something = `Hello World,
+Welcome to the repository
+Thank You For Visiting`;
+
+console.log(something);
