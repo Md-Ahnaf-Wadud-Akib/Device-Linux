93cb178a24d40fb805b301157bb7f6011488d1e0
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-10-25 17:21:26 +0600

    spread

--- upgrade/javaScript/spread.js
+++ upgrade/javaScript/spread.js
@@ -1,15 +1,18 @@
-function maxValue(array) {
-  maxElement = 0;
-  for (i = 0; i <= array.length; i++) {
-    let index = i;
-    let element = array[index];
-    if (element > maxElement) {
-      maxElement = element;
-    }
-  }
-  return maxElement;
-}
+// function maxValue(array) {
+//   maxElement = 0;
+//   for (i = 0; i <= array.length; i++) {
+//     let index = i;
+//     let element = array[index];
+//     if (element > maxElement) {
+//       maxElement = element;
+//     }
+//   }
+//   return maxElement;
+// }
 
 const abcArray = [23, 97, 56, 74, 568, 78, 54, 37, 26];
-const result = maxValue(abcArray);
-console.log(`The max value of ${abcArray} is: ${result}`);
+const maxValue = Math.max(...abcArray);
+console.log(maxValue);
+// const result = maxValue(abcArray);
+// console.log(`The max value of ${abcArray} is: ${result}`);
+
