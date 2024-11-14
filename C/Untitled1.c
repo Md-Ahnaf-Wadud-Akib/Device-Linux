d174820c2deee30ac75b8ef4106dc616d8b57e53
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-11-14 18:39:00 +0600

    C

---
+++ C/Untitled1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int price = 23;
+    int quantity = 3;
+
+    int total = price * quantity;
+
+    printf("%d", total);
+}
---
+++ C/operator.c
@@ -0,0 +1,25 @@
+//#include<stdio.h>
+//
+//int main() {
+//    int a = 2;
+//    int b = 5;
+//    int s = a + b;
+//    printf("%d", s);
+//};
+
+
+#include<stdio.h>
+
+int main() {
+    int price = 100;
+    printf("%d\n", price);
+
+    float tep = 30.5;
+    printf("%f\n", tep);
+
+    double temp = 36.6;
+    printf("%lf\n", temp);
+
+    char name = 'f';
+    printf("%c", name);
+}
