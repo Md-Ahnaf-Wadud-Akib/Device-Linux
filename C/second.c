90ccdb2cd27ac2a84c9f974069ec39253c244805
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-11-16 21:55:50 +0600

    C

---
+++ C/second.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int num1, num2, sum;
+    printf("Please enter the first number: ");
+    scanf("%d", &num1);
+
+    printf("Please enter the second number: ");
+    scanf("%d", &num2);
+
+    sum = num1 + num2;
+
+    printf("The sum is: %d", sum);
+
+}
