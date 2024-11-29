19e7fc40995e6c01bf2548dce19d5aa5f5d8cefa
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-11-29 19:47:00 +0600

    map

--- upgrade/javaScript/map.js
+++ upgrade/javaScript/map.js
@@ -6,4 +6,16 @@ function doubleIt(num) {
 
 const result = numbers.map(doubleIt);
 
-console.log(`the result is ${result}`);
+// console.log(`the result is ${result}`);
+
+const double2 = (n) => n * 2;
+
+const result2 = numbers.map(double2);
+// console.log(`the resulted array is: ${result}`);
+
+const num = [23, 45, 78, 93, 45];
+
+const divBy2 = (n) => n / 2;
+
+const result3 = num.map(divBy2);
+// console.log(result3);
