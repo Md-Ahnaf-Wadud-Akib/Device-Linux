43023e65ad6bd3495dd5c7a4488bd7e770d6a96d
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-11-29 22:14:52 +0600

    foreach

--- upgrade/javaScript/foreach.js
+++ upgrade/javaScript/foreach.js
@@ -17,4 +17,19 @@ const selected = players.filter((p) => p > 50);
 const cats = [2, 3, 4, 5, 6, 7, 8, 9];
 
 // console.log(cats.map((p) => p));
-console.log(cats.filter((c) => c % 2 !== 0));
+// console.log(cats.filter((c) => c % 2 !== 0));
+
+const products = [
+  { id: 1, title: "Samsung", price: 20000 },
+  { id: 2, title: "Iphone", price: 50000 },
+  { id: 3, title: "Vivo", price: 30000 },
+  { id: 4, title: "Oppo", price: 12000 },
+];
+
+const result1 = products.map((product) => product.title);
+// console.log(result1);
+
+const result2 = products.filter((p) => {
+  let t = p * 2;
+  console.log(t);
+});
