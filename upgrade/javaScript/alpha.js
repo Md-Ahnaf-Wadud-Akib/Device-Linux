a8bae95b6d351b1e835eb14bac4508d06b165568
Md-Ahnaf-Wadud-Akib <ahnafakib009@gmail.com>
2024-12-20 23:12:07 +0600

    alpha-2

--- upgrade/javaScript/alpha.js
+++ upgrade/javaScript/alpha.js
@@ -13,4 +13,32 @@ function smallestNumber(inputArray) {
 
 let abcArray = [23, 56, 93, 104, 567];
 let result = smallestNumber(abcArray);
-console.log(`The result is ${result}`);
+// console.log(`The result is ${result}`);
+
+function sumOfArray(inputArray2) {
+  let sum = 0;
+  for (let i = 0; i <= inputArray2; i++) {
+    let element2 = inputArray2[i];
+    sum += element2;
+  }
+  return sum;
+}
+
+let abcArray2 = [2, 3, 5, 9, 10];
+let result2 = sumOfArray(abcArray2);
+// console.log(`The result is: ${result2}`);
+
+function largestNumber(inputArray3) {
+  let value = 0;
+  for (let i = 0; i <= inputArray3.length; i++) {
+    let element = inputArray3[i];
+    if (element > value) {
+      value = element;
+    }
+  }
+  return value;
+}
+
+let abcArray3 = [2, 3, 5, 6, 9];
+let result3 = largestNumber(abcArray3);
+console.log(`The largest number is: ${result3}`);
